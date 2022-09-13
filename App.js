1916942e40d7d6be1dc60adcc93f6921bc9f9b27
Android Studio <devandroid@ort>
2022-09-13 08:58:34 -0300

    proyecto terminado

--- App.js
+++ App.js
@@ -1,12 +1,36 @@
+import {React, useState} from "react";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Alert } from 'react-native';
 
 export default function App() {
+
+  const [showText, setshowText]= useState("");
+  const [text, setText] = useState("");
+
   return (
+    
+    <>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>hola polshu, contate algo</Text>
       <StatusBar style="auto" />
+      <TextInput
+        style={styles.input}
+        value={Text}
+        onChangeText={setText}
+        placeholder="contame aca         "
+      />
+      <Button
+        title="Apretame"
+        onPress={() => {setshowText(text); setText("")}}
+      />
+      <Text>
+      {showText}
+    </Text>
     </View>
+    
+    
+    </>
+    
   );
 }
 
@@ -16,5 +40,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
